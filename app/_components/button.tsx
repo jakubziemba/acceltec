68ea10ef622dcb99a076b642fcf37f1035a1e89a
Jakub Ziemba <ziemba.jak@gmail.com>
2024-04-12 01:12:53 +0200

    (fix) remove outline from button & add focus state

--- app/_components/button.tsx
+++ app/_components/button.tsx
@@ -6,7 +6,7 @@ export default function Button({ children, ...props }: { children: React.ReactNo
   return (
     <motion.button
       // layoutId="form-button"
-      className="my-32 px-6 py-3 text-xl text-white/80 rounded-[32px] shadow-[0px_0px_0px_2px_hsla(0,0%,100%,0.2),0px_2px_2px_0px_hsla(0,0%,0%,0.2)] hover:shadow-[0px_0px_0px_2px_hsla(0,0%,100%,1),0px_2px_2px_0px_hsla(0,0%,0%,0.2)] transition duration-[200ms] ease-in hover:text-white active:scale-[0.99] hover:scale-[1.01]"
+      className="my-32 px-6 py-3 text-xl text-white/80 rounded-[32px] shadow-[0px_0px_0px_2px_hsla(0,0%,100%,0.2),0px_2px_2px_0px_hsla(0,0%,0%,0.2)] hover:shadow-[0px_0px_0px_2px_hsla(0,0%,100%,1),0px_2px_2px_0px_hsla(0,0%,0%,0.2)] focus:shadow-[0px_0px_0px_2px_hsla(0,0%,100%,1),0px_2px_2px_0px_hsla(0,0%,0%,0.2)] transition duration-[200ms] ease-in outline-none hover:text-white active:scale-[0.99] hover:scale-[1.01]"
       {...props}
     >
       {children}
