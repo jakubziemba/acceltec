c641c264ec41f95613a3adb92297a8c7de101f95
Jakub Ziemba <ziemba.jak@gmail.com>
2024-04-19 16:39:51 +0200

    (fix) disable vercel toolbar

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async Headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "x-vercel-skip-toolbar",
+            value: "1",
+          },
+        ],
+      },
+    ];
+  },
+};
 
 export default nextConfig;
