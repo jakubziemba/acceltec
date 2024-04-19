f93244a0f9287f5647a9f0948167ad34406aa909
Jakub Ziemba <ziemba.jak@gmail.com>
2024-04-19 12:25:06 +0200

    (wip) button to form animation

--- app/_components/form/form.tsx
+++ app/_components/form/form.tsx
@@ -4,7 +4,7 @@ import { forwardRef } from "react";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import * as z from "zod";
-import { motion } from "framer-motion";
+import { MotionConfig, motion } from "framer-motion";
 import LogoCard from "./logo-card";
 
 const formSchema = z.object({
@@ -55,84 +55,94 @@ const Form = (
       }}
       layout
       layoutId="form-button"
-      className="mx-auto w-full max-w-4xl origin-top scroll-mt-10"
+      className="mx-auto w-full max-w-4xl origin-top -scroll-mt-10"
     >
       <div className="mx-auto flex max-w-xl flex-col gap-6 rounded-[30px] bg-[#171717] p-4 lg:max-w-full lg:flex-row lg:p-7">
-        <motion.div
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          exit={{ opacity: 0 }}
-          transition={{ type: "linear", duration: 0.1 }}
-          className="lg:flex-2 flex flex-col gap-2 text-base leading-10 tracking-wide lg:w-full lg:max-w-md lg:text-xl"
+        <MotionConfig
+          transition={{
+            type: "linear",
+            delay: 0.15,
+            duration: 0.25,
+            ease: "easeIn",
+          }}
         >
-          <label
-            htmlFor="name"
-            className="w-full rounded-[18px] px-4 py-3 leading-10 tracking-wide text-white/50 lg:text-lg"
+          <motion.div
+            // initial={{ opacity: 0 }}
+            // animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            className="lg:flex-2 flex flex-col gap-2 text-base leading-10 tracking-wide lg:w-full lg:max-w-md lg:text-xl"
           >
-            From
-          </label>
-          <div className="group/name relative flex flex-col gap-2">
-            <input
-              id="name"
-              type="text"
-              placeholder="Name"
-              autoComplete="off"
-              className="peer bg-inherit px-4 py-2 leading-10 outline-none transition duration-200 placeholder:tracking-wide placeholder:text-white/20"
-              {...register("name")}
-            />
-            <div className="pointer-events-none absolute bottom-0 left-0 h-px w-full rounded-lg bg-white/5 transition-colors duration-200 group-hover/name:bg-white/35 peer-focus-within:bg-white" />
-          </div>
-          {errors?.name && (
-            <p className="px-1 text-xs text-red-600">{errors.name.message}</p>
-          )}
-
-          <div className="group/email relative flex flex-col gap-2">
-            <input
-              id="email"
-              type="email"
-              placeholder="Email"
-              autoComplete="off"
-              className="peer bg-inherit px-4 py-2 leading-10 outline-none transition-colors duration-200 placeholder:tracking-wide placeholder:text-white/20"
-              {...register("email")}
-            />
-            <div className="pointer-events-none absolute bottom-0 left-0 h-px w-full rounded-lg bg-white/5 transition-colors duration-200 group-hover/email:bg-white/35 peer-focus-visible:bg-white" />
-          </div>
-          {errors?.email && (
-            <p className="px-1 text-xs text-red-600">{errors.email.message}</p>
-          )}
-          <div className="group/content relative flex flex-col gap-2">
-            <textarea
-              id="content"
-              placeholder="My project is about..."
-              className="peer h-32 resize-none bg-inherit px-4 py-2 leading-6 outline-none placeholder:tracking-wide placeholder:text-white/20 lg:h-72"
-              {...register("content")}
-            />
-            <div className="pointer-events-none absolute bottom-0 left-0 h-px w-full rounded-lg bg-white/5 transition-colors duration-200 group-hover/content:bg-white/35 peer-focus-visible:bg-white" />
-          </div>
-          {errors?.content && (
-            <p className="px-1 text-xs text-red-600">
-              {errors.content.message}
-            </p>
-          )}
-          <button className="rounded-[44px] bg-white/10 px-4 py-1 text-base leading-10 text-white/50 outline-none transition duration-200 focus-visible:scale-[0.98] lg:w-max lg:rounded-xl lg:bg-inherit lg:text-xl lg:text-white lg:hover:scale-[1.02] lg:active:scale-[0.98]">
-            Send
-          </button>
-        </motion.div>
-        <motion.div
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          transition={{ type: "linear", duration: 0.1 }}
-          className="relative isolate w-full overflow-hidden rounded-[18px] bg-white/5 lg:flex-1"
-        >
-          <div className="flex h-auto min-h-48 w-full flex-col justify-between px-6 pb-4 pt-2 tracking-wide lg:absolute lg:inset-0">
-            <p className="text-lg leading-10 text-white/50">To</p>
-            <div className="text-2xl leading-10">
-              <p className="text-white">Laurence Laumann</p>
-              <p className="text-xl text-white/70">Founder</p>
+            <label
+              htmlFor="name"
+              className="w-full rounded-[18px] px-4 py-3 leading-10 tracking-wide text-white/50 lg:text-lg"
+            >
+              From
+            </label>
+            <div className="group/name relative flex flex-col gap-2">
+              <input
+                id="name"
+                type="text"
+                placeholder="Name"
+                autoComplete="off"
+                className="peer bg-inherit px-4 py-2 leading-10 outline-none transition duration-200 placeholder:tracking-wide placeholder:text-white/20"
+                {...register("name")}
+              />
+              <div className="pointer-events-none absolute bottom-0 left-0 h-px w-full rounded-lg bg-white/5 transition-colors duration-200 group-hover/name:bg-white/35 peer-focus-within:bg-white" />
             </div>
-          </div>
-          <LogoCard className="absolute -top-96 left-36 -z-10 h-full w-full select-none lg:-left-7 lg:-top-[360px]" />
-        </motion.div>
+            {errors?.name && (
+              <p className="px-1 text-xs text-red-600">{errors.name.message}</p>
+            )}
+
+            <div className="group/email relative flex flex-col gap-2">
+              <input
+                id="email"
+                type="email"
+                placeholder="Email"
+                autoComplete="off"
+                className="peer bg-inherit px-4 py-2 leading-10 outline-none transition-colors duration-200 placeholder:tracking-wide placeholder:text-white/20"
+                {...register("email")}
+              />
+              <div className="pointer-events-none absolute bottom-0 left-0 h-px w-full rounded-lg bg-white/5 transition-colors duration-200 group-hover/email:bg-white/35 peer-focus-visible:bg-white" />
+            </div>
+            {errors?.email && (
+              <p className="px-1 text-xs text-red-600">
+                {errors.email.message}
+              </p>
+            )}
+            <div className="group/content relative flex flex-col gap-2">
+              <textarea
+                id="content"
+                placeholder="My project is about..."
+                className="peer h-32 resize-none bg-inherit px-4 py-2 leading-6 outline-none placeholder:tracking-wide placeholder:text-white/20 lg:h-72"
+                {...register("content")}
+              />
+              <div className="pointer-events-none absolute bottom-0 left-0 h-px w-full rounded-lg bg-white/5 transition-colors duration-200 group-hover/content:bg-white/35 peer-focus-visible:bg-white" />
+            </div>
+            {errors?.content && (
+              <p className="px-1 text-xs text-red-600">
+                {errors.content.message}
+              </p>
+            )}
+            <button className="rounded-[44px] bg-white/10 px-4 py-1 text-base leading-10 text-white/50 outline-none transition duration-200 focus-visible:scale-[0.98] lg:w-max lg:rounded-xl lg:bg-inherit lg:text-xl lg:text-white lg:hover:scale-[1.02] lg:active:scale-[0.98]">
+              Send
+            </button>
+          </motion.div>
+          <motion.div
+            // initial={{ opacity: 0 }}
+            // animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            className="relative isolate w-full overflow-hidden rounded-[18px] bg-white/5 lg:flex-1"
+          >
+            <div className="flex h-auto min-h-48 w-full flex-col justify-between px-6 pb-4 pt-2 tracking-wide lg:absolute lg:inset-0">
+              <p className="text-lg leading-10 text-white/50">To</p>
+              <div className="text-2xl leading-10">
+                <p className="text-white">Laurence Laumann</p>
+                <p className="text-xl text-white/70">Founder</p>
+              </div>
+            </div>
+            <LogoCard className="absolute -top-96 left-36 -z-10 h-full w-full select-none lg:-left-7 lg:-top-[360px]" />
+          </motion.div>
+        </MotionConfig>
       </div>
     </motion.form>
   );
--- app/_components/form/section.tsx
+++ app/_components/form/section.tsx
@@ -2,9 +2,8 @@
 
 import { useState, useRef, useEffect } from "react";
 import {
-  AnimatePresence,
+  LayoutGroup,
   motion,
-  useInView,
   useMotionValueEvent,
   useScroll,
 } from "framer-motion";
@@ -14,36 +13,29 @@ import AnimatedText from "../animated-text";
 
 export default function FormSection() {
   const containerRef = useRef<null | HTMLDivElement>(null);
-  const buttonRef = useRef<null | HTMLButtonElement>(null);
   const formRef = useRef<null | HTMLFormElement>(null);
   const [showForm, setShowForm] = useState(false);
   const [shouldScroll, setShouldScroll] = useState(false);
+
+  const { scrollYProgress } = useScroll({
+    target: containerRef,
+    offset: ["0% end", "end end"],
+  });
 
   function handleButtonClick() {
     setShowForm(true);
     setShouldScroll(true);
   }
 
-  const { scrollY, scrollYProgress } = useScroll({
-    target: containerRef,
-    offset: ["0% end", "end end"],
-  });
-
-  const containerInView = useInView(containerRef, {});
-
-  const buttonInView = useInView(buttonRef, {
-    margin: "-25%",
-  });
-
   useMotionValueEvent(scrollYProgress, "change", (value) => {
-    if (value > 0.85) {
+    if (value > 0.8) {
       setShowForm(true);
       setShouldScroll(false);
     }
 
     if (shouldScroll) return;
 
-    if (value < 0.85) {
+    if (value < 0.8) {
       setShowForm(false);
     }
   });
@@ -52,111 +44,116 @@ export default function FormSection() {
     if (!shouldScroll || !formRef.current) return;
 
     formRef.current?.scrollIntoView({
-      block: "start",
+      block: "end",
       behavior: "smooth",
     });
   }, [shouldScroll]);
 
   return (
-    <>
-      <motion.section
-        ref={containerRef}
-        className={tw(
-          "relative mx-auto mt-20 min-h-screen w-full max-w-xl origin-center space-y-20 px-6 [perspective:45px] lg:max-w-4xl lg:px-0 ",
-        )}
-      >
-        <motion.div
-          initial={{ opacity: 1, translateZ: "0px" }}
-          animate={{
-            // scale: showForm ? 0.95 : 1,
-            opacity: showForm ? 0.1 : 1,
-            translateZ: showForm ? "-3px" : "0px",
-          }}
-          transition={{
-            duration: 0.2,
-            opacity: { type: "linear", duration: 0.2 },
-          }}
-          className="sticky top-20 mx-auto flex flex-col space-y-8 text-balance text-2xl leading-6 [perspective:1000px] xs:[text-wrap:initial] lg:space-y-6 lg:text-4xl lg:leading-10"
+    <div ref={containerRef} className="relative">
+      <LayoutGroup>
+        <motion.section
+          className={tw(
+            "relative mx-auto w-full max-w-xl origin-center space-y-20 px-6 pt-20 [perspective:45px] lg:max-w-4xl lg:px-0 ",
+          )}
         >
-          <AnimatedText el="h2" className="origin-bottom text-white/80">
-            Berlin-based software studio that exclusively works with founders,
-            executives and innovative teams.
-          </AnimatedText>
-          <AnimatedText className="text-white/40">
-            We partner with companies and founders who aren’t afraid to
-            challenge conventional order. We invest our time, resources and
-            networks in those who recognize the value we bring to the table.
-          </AnimatedText>
-          <AnimatedText className="text-white/40">
-            We work in a research driven manner. We try to ask the right
-            questions to understand and uncover the problem space. We identify
-            the nail before we start worrying about the hammer.
-          </AnimatedText>
-          <div className="flex w-full justify-center pt-10">
-            {!showForm && (
-              <motion.button
-                ref={buttonRef}
-                onClick={handleButtonClick}
-                layoutId="form-button"
-                layout
-                initial={{
-                  translateZ: 0,
-                  boxShadow:
-                    "0px 0px 0px 2px rgba(255, 255, 255, 0.2), 0px 2px 2px 0px rgba(0, 0, 0, 0.2)",
-                }}
-                animate={{
-                  translateZ: 0,
-                  boxShadow:
-                    "0px 0px 0px 2px rgba(255, 255, 255, 0.2), 0px 2px 2px 0px rgba(0, 0, 0, 0.2)",
-                }}
-                exit={{
-                  translateZ: 0,
-                  boxShadow:
-                    "0px 0px 0px 2px rgba(255, 255, 255, 0), 0px 2px 2px 0px rgba(0, 0, 0, 0)",
-                }}
-                whileInView={{ scale: 1.25, translateZ: 0 }}
-                whileHover={{
-                  boxShadow:
-                    "0px 0px 0px 2px rgba(255, 255, 255, 1), 0px 2px 2px 0px rgba(0, 0, 0, 0.2)",
-                  color: "white",
-                }}
-                viewport={{ margin: "-20%" }}
-                transition={{
-                  type: "spring",
-                  damping: 5,
-                  mass: 0.01,
-                  bounce: 0,
-                }}
-                className="origin-top select-none self-center rounded-[32px] px-6 py-3 text-xl text-white/80 outline-none focus-visible:shadow-[0px_0px_0px_2px_hsla(0,0%,100%,1),0px_2px_2px_0px_hsla(0,0%,0%,0.2)]"
-              >
-                <motion.span
-                  initial={{ opacity: 0 }}
-                  animate={{ opacity: 1 }}
-                  exit={{ opacity: 0 }}
-                  transition={{ duration: 0.3, delay: showForm ? 0 : 0.3 }}
+          <motion.div
+            initial={{ opacity: 1, translateZ: "0px", y: 0 }}
+            animate={{
+              opacity: showForm ? 0.1 : 1,
+              translateZ: showForm ? "-3px" : "0px",
+            }}
+            transition={{
+              duration: 0.2,
+              opacity: { type: "linear", duration: 0.2 },
+            }}
+            className="sticky top-20 mx-auto flex flex-col space-y-8 text-balance text-2xl leading-6 [perspective:1000px] xs:[text-wrap:initial] lg:space-y-6 lg:text-4xl lg:leading-10"
+          >
+            <AnimatedText el="h2" className="origin-bottom text-white/80">
+              Berlin-based software studio that exclusively works with founders,
+              executives and innovative teams.
+            </AnimatedText>
+            <AnimatedText className="text-white/40">
+              We partner with companies and founders who aren’t afraid to
+              challenge conventional order. We invest our time, resources and
+              networks in those who recognize the value we bring to the table.
+            </AnimatedText>
+            <AnimatedText className="text-white/40">
+              We work in a research driven manner. We try to ask the right
+              questions to understand and uncover the problem space. We identify
+              the nail before we start worrying about the hammer.
+            </AnimatedText>
+            <div className="flex w-full justify-center pt-10">
+              {!showForm && (
+                <motion.button
+                  onClick={handleButtonClick}
+                  layoutId="form-button"
+                  layout
+                  initial={{
+                    translateZ: 0,
+                    boxShadow:
+                      "0px 0px 0px 2px rgba(255, 255, 255, 0.2), 0px 2px 2px 0px rgba(0, 0, 0, 0.2)",
+                  }}
+                  animate={{
+                    translateZ: 0,
+                    boxShadow:
+                      "0px 0px 0px 2px rgba(255, 255, 255, 0.2), 0px 2px 2px 0px rgba(0, 0, 0, 0.2)",
+                  }}
+                  exit={{
+                    translateZ: 0,
+                    boxShadow:
+                      "0px 0px 0px 2px rgba(255, 255, 255, 0), 0px 2px 2px 0px rgba(0, 0, 0, 0)",
+                  }}
+                  whileInView={{ scale: 1.25, translateZ: 0 }}
+                  whileHover={{
+                    boxShadow:
+                      "0px 0px 0px 2px rgba(255, 255, 255, 1), 0px 2px 2px 0px rgba(0, 0, 0, 0.2)",
+                    color: "rgba(255, 255, 255, 1)",
+                  }}
+                  viewport={{ margin: "-20%" }}
+                  transition={{
+                    type: "spring",
+                    damping: 5,
+                    mass: 0.01,
+                    bounce: 0,
+                  }}
+                  className="relative z-[9999] origin-top select-none self-center rounded-[32px] px-6 py-3 text-xl text-white/80 outline-none focus-visible:shadow-[0px_0px_0px_2px_hsla(0,0%,100%,1),0px_2px_2px_0px_hsla(0,0%,0%,0.2)]"
                 >
-                  Pitch your project
-                </motion.span>
-              </motion.button>
-            )}
-          </div>
-        </motion.div>
-      </motion.section>
-      {showForm && (
+                  <motion.span
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    exit={{ opacity: 0 }}
+                    transition={{
+                      type: "linear",
+                      duration: 0.3,
+                      ease: "easeIn",
+                      delay: showForm ? 0 : 0.3,
+                    }}
+                  >
+                    Pitch your project
+                  </motion.span>
+                </motion.button>
+              )}
+            </div>
+          </motion.div>
+        </motion.section>
         <motion.div
           key="pitch"
           layout
-          // initial={{ y: "0%" }}
-          // animate={{ y: showForm ? "40%" : "0%" }}
-          exit={{ scale: 0 }}
-          transition={{
-            duration: 0.25,
+          initial={{ position: "relative", bottom: "0%" }}
+          animate={{
+            position: showForm ? "sticky" : "relative",
+            y: showForm ? -160 : 0,
+            bottom: "2%",
           }}
-          className="fixed bottom-20 flex w-full flex-col items-center"
+          exit={{ opacity: 0 }}
+          className="relative flex h-[476px] w-full flex-col items-center px-4"
         >
-          <Form ref={formRef} showForm={showForm} />
+          <div className="sticky top-0 w-full">
+            {showForm && <Form ref={formRef} showForm={showForm} />}
+          </div>
         </motion.div>
-      )}
-    </>
+      </LayoutGroup>
+    </div>
   );
 }
