835dc8525b734577c3b6d51b57eb35e25e5cf5a6
Jakub Ziemba <ziemba.jak@gmail.com>
2024-04-23 14:26:32 +0200

    (fix) remove unused import

--- app/_components/button-home.tsx
+++ app/_components/button-home.tsx
@@ -2,7 +2,7 @@
 
 import Link from "next/link";
 import { motion, useMotionValueEvent, useScroll } from "framer-motion";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 export default function ButtonHome({
   children,
