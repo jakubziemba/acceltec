ba1ba756f06ab95268fe07bf2b54a0e30a30268a
Jakub Ziemba <ziemba.jak@gmail.com>
2024-05-01 19:06:31 +0200

    (fix) styling

--- app/(legal-pages)/layout.tsx
+++ app/(legal-pages)/layout.tsx
@@ -7,7 +7,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
       <nav className="fixed top-10 z-10 flex w-full justify-center lg:top-20">
         <ButtonHome>Home</ButtonHome>
       </nav>
-      <section className="flex min-h-[calc(100svh-80px)] w-full max-w-xl flex-col gap-6 px-6 py-20 text-left lg:max-w-4xl lg:px-0 lg:py-32">
+      <section className="flex min-h-[calc(100svh-80px)] w-full max-w-xl flex-col gap-6 px-6 py-32 text-left lg:max-w-4xl lg:px-0">
         <div className="flex w-full flex-col justify-center gap-6 py-4">
           {children}
         </div>
