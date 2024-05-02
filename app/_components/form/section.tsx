87a3216caad42d1a3eecd8bbe3853e7e3c690f00
Jakub Ziemba <ziemba.jak@gmail.com>
2024-05-02 20:42:11 +0200

    (fix) about mobile text size

--- app/_components/form/section.tsx
+++ app/_components/form/section.tsx
@@ -170,7 +170,7 @@ export default function FormSection() {
         ref={sectionRef}
         className="relative mt-[80lvh] h-[160vh] w-full [perspective:45px] lg:mt-[100lvh]"
       >
-        <div className="sticky top-0 mx-auto flex h-screen w-full max-w-xl origin-center flex-col items-center justify-center text-balance px-6 text-base leading-5 [perspective:45px] supports-[height:100lvh]:h-lvh max-2xs:text-lg max-2xs:leading-6 max-[350px]:text-sm max-[350px]:leading-4 xs:[text-wrap:initial] md:text-2xl md:leading-6 lg:max-w-4xl lg:px-0 lg:pt-0 lg:text-4xl lg:leading-10">
+        <div className="sticky top-0 mx-auto flex h-screen w-full max-w-xl origin-center flex-col items-center justify-center px-6 text-lg leading-5 [perspective:45px] supports-[height:100lvh]:h-lvh max-2xs:text-lg max-2xs:leading-5 max-[350px]:text-sm max-[350px]:leading-4 xs:[text-wrap:initial] md:text-2xl md:leading-6 lg:max-w-4xl lg:px-0 lg:pt-0 lg:text-4xl lg:leading-10">
           <motion.div
             initial={{ z: 0 }}
             animate={{
