42116f44f0dbe881cf3bb8dfa35d7c9763fb5c57
Jakub Ziemba <ziemba.jak@gmail.com>
2024-05-06 20:23:39 +0200

    (fix) cleanup

--- app/_components/lenis-wrapper.tsx
+++
@@ -1,9 +0,0 @@
-import { Lenis } from "@/libs/react-lenis";
-
-export default function LenisWrapper({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return <Lenis root>{children}</Lenis>;
-}
